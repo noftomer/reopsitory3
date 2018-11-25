8a0dde206f7381dfa4a8791747b1604c0aac53dc
tomernof <tomnof81@gmail.com>
2018-11-25 21:59:35 +0200

    first

---
+++ testNewEclipse/src/testNewEclipse/TestSelenium.java
@@ -0,0 +1,25 @@
+package testNewEclipse;
+
+import org.junit.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestSelenium {
+	static WebDriver driver;
+	@BeforeClass
+	public static void start() {
+		System.out.println("start");
+		System.setProperty("webdriver.chrome.driver","C:\\SeleniumDrivers\\chromedriver_win32\\chromedriver_original.exe");
+		driver=new ChromeDriver();
+	}
+	@Test
+	public void t1() {
+		driver.get("https://www.youtube.com/");
+		System.out.println(driver.getTitle());
+	}
+	@AfterClass
+	public static void end() {
+		driver.quit();
+		System.out.println("end");
+	}
+}
